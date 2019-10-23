b2bcf8f2c99572abd6d48149c0cb38da7909542c
lothrazar <samson.bassett@gmail.com>
2019-10-23 00:05:26 -0700

    1.0.3 refactor and cleanup

--- src/main/java/com/lothrazar/autorun/AutoSprintMod.java
+++ src/main/java/com/lothrazar/autorun/AutoSprintMod.java
@@ -5,10 +5,6 @@ import com.lothrazar.autorun.setup.IProxy;
 import com.lothrazar.autorun.setup.ServerProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
@@ -22,12 +18,11 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("autorun")
-public class ExampleMod {
+public class AutoSprintMod {
 
-  private static final String NBT = "isautorunning";
   public static final IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
 
-  public ExampleMod() {
+  public AutoSprintMod() {
     // Register the setup method for modloading
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     MinecraftForge.EVENT_BUS.register(this);
@@ -40,56 +35,28 @@ public class ExampleMod {
   @OnlyIn(Dist.CLIENT)
   @SubscribeEvent
   public void onTick(PlayerTickEvent event) {
-    PlayerEntity p = event.player;
-    if (p.getPersistentData().getBoolean(NBT)) {
-      this.move(p);
+    if (AutoSprintUtil.getAutorunState(event.player)) {
+      AutoSprintUtil.move(event.player);
     }
-  }
-
-  @OnlyIn(Dist.CLIENT)
-  private void move(PlayerEntity p) {
-    p.moveForward = 0.85F;
-    Vec3d p_213352_1_ = new Vec3d(p.moveStrafing, p.moveVertical, p.moveForward);
-    World world = p.world;
-    BlockPos blockpos = new BlockPos(p.posX, p.getBoundingBox().minY - 1.0D, p.posZ);
-    float f5 = p.world.getBlockState(blockpos).getSlipperiness(world, blockpos, p);
-    p.moveRelative(func_213335_r(p, f5), p_213352_1_);
-  }
-
-  /**
-   * LivingEntity.class::2091
-   * 
-   * @param p
-   * @param p_213335_1_
-   * @return
-   */
-  @OnlyIn(Dist.CLIENT)
-  private float func_213335_r(PlayerEntity p, float p_213335_1_) {
-    return p.onGround ? p.getAIMoveSpeed() * (0.21600002F / (p_213335_1_ * p_213335_1_ * p_213335_1_)) : p.jumpMovementFactor;
   }
 
   @OnlyIn(Dist.CLIENT)
   @SubscribeEvent
   public void onKeyInput(InputEvent.KeyInputEvent event) {
     PlayerEntity player = proxy.getClientPlayer();
-    boolean isCurrentlyAutorun = player.getPersistentData().getBoolean(NBT);
+    boolean isCurrentlyAutorun = AutoSprintUtil.getAutorunState(player);
     if (ClientProxy.key != null && ClientProxy.key.isPressed()) {
-      //toggle it off
-      setAutorunState(player, !isCurrentlyAutorun);
+      //toggle it to inverse
+      AutoSprintUtil.setAutorunState(player, !isCurrentlyAutorun);
     }
     else if (isCurrentlyAutorun) {
       if (Minecraft.getInstance().gameSettings.keyBindForward.isKeyDown() ||
           Minecraft.getInstance().gameSettings.keyBindBack.isKeyDown() ||
           Minecraft.getInstance().gameSettings.keyBindLeft.isKeyDown() ||
           Minecraft.getInstance().gameSettings.keyBindRight.isKeyDown()) {
-        setAutorunState(proxy.getClientPlayer(), false);
+        //auto off
+        AutoSprintUtil.setAutorunState(player, false);
       }
     }
-  }
-
-  @OnlyIn(Dist.CLIENT)
-  public static void setAutorunState(PlayerEntity player, boolean value) {
-    player.getPersistentData().putBoolean(NBT, value);
-    player.sendStatusMessage(new TranslationTextComponent("autorun." + value), true);
   }
 }
---
+++ src/main/java/com/lothrazar/autorun/AutoSprintUtil.java
@@ -0,0 +1,49 @@
+package com.lothrazar.autorun;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class AutoSprintUtil {
+
+  private static final String NBT = "isautorunning";
+
+  /**
+   * Do the auto sprint thing
+   * 
+   * @param p
+   */
+  public static void move(PlayerEntity p) {
+    p.moveForward = 0.85F;
+    Vec3d p_213352_1_ = new Vec3d(p.moveStrafing, p.moveVertical, p.moveForward);
+    World world = p.world;
+    BlockPos blockpos = new BlockPos(p.posX, p.getBoundingBox().minY - 1.0D, p.posZ);
+    float f5 = p.world.getBlockState(blockpos).getSlipperiness(world, blockpos, p);
+    p.moveRelative(AutoSprintUtil.func_213335_r(p, f5), p_213352_1_);
+  }
+
+  /**
+   * LivingEntity.class::2091
+   * 
+   * @param p
+   * @param p_213335_1_
+   * @return
+   */
+  public static float func_213335_r(PlayerEntity p, float p_213335_1_) {
+    return p.onGround ? p.getAIMoveSpeed() * (0.21600002F / (p_213335_1_ * p_213335_1_ * p_213335_1_)) : p.jumpMovementFactor;
+  }
+
+  public static void setAutorunState(PlayerEntity player, boolean value) {
+    player.getPersistentData().putBoolean(NBT, value);
+    player.sendStatusMessage(new TranslationTextComponent("autorun." + value), true);
+  }
+
+  public static boolean getAutorunState(PlayerEntity player) {
+    return player.getPersistentData().getBoolean(NBT);
+  }
+}
